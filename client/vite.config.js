93aef9cbaf68080f949e7dd95c5c381d428700a8
«JarneTek» <jarnteki@student.arteveldehs.be>
2025-06-03 14:59:56 +0200

    Fix Netlify deployment: simplified TanStack Router plugin config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-    react()],
+    TanStackRouterVite(),
+    react()
+  ],
 })
